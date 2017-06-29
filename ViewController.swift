f1887b338072ae90baf3eb65dcb4b430d5861423
FaithDeveloper <dev.faithpeople@gmail.com>
2017-06-29 14:10:13 +0900

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoginModel
+//
+//  Created by sigong_shin on 2017. 6. 9..
+//  Copyright © 2017년 kcs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
